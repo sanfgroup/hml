7cb1813608efb0721045fb455981229ae278c5f9
sanfgroup <sanfgroup@gmail.com>
2013-10-22 13:39:28 +0300

    update

---
+++ app/lang/ru/admin/roles/title.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+
+	'role_management'    => 'Управление группами',
+	'role_update'        => 'Обновление группы',
+	'role_delete'        => 'Удаление группы',
+	'create_a_new_role'  => 'Создать новую группу',
+
+);
---
+++ app/lang/ru/admin/users/table.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+
+	'first_name' => 'Имя',
+	'last_name'  => "Фамилия",
+	'user_id'  => 'ID пользователя',
+	'username'  => 'Логин',
+	'email'      => 'Адрес',
+	'groups'     => 'Группы',
+	'roles'     => 'Группы',
+	'activated'  => 'Активирован',
+	'created_at' => 'Зарегистрирован с',
+
+);
