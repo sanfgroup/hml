c8f2073976d957b9e23101b7283070d8746134bd
sanfgroup <sanfgroup@gmail.com>
2013-10-22 17:19:01 +0300

    update

--- app/config/packages/jasonlewis/basset/config.php
+++ app/config/packages/jasonlewis/basset/config.php
@@ -56,7 +56,7 @@ return array(
             $collection->directory('assets/js', function($collection)
             {
                 $collection->javascript('//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js');
-                //$collection->add('bootstrap/bootstrap.js');
+                $collection->add('bootstrap/bootstrap.js');
                 $collection->requireDirectory('../../../vendor/twbs/bootstrap/js');
                 $collection->add('wysihtml5/wysihtml5-0.3.0.js');
                 $collection->add('wysihtml5/bootstrap-wysihtml5.js');
