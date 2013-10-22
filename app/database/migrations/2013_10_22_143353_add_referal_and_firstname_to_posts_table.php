04dee0e8605fd0893ab22a995309619c648dac29
sanfgroup <sanfgroup@gmail.com>
2013-10-22 17:39:17 +0300

    update

---
+++ app/database/migrations/2013_10_22_143353_add_referal_and_firstname_to_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddReferalAndFirstnameToPostsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('posts', function(Blueprint $table) {
+			$table->string('first_name');
+			$table->string('last_name');
+			$table->string('perfect_money');
+			$table->string('okpay');
+			$table->integer('money');
+			$table->string('perfect_money');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('posts', function(Blueprint $table) {
+			
+		});
+	}
+
+}
