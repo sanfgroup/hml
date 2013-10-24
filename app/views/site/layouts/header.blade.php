d55a7d2b105b9e8da5d8bd78e315860abce156bb
sanfgroup <sanfgroup@gmail.com>
2013-10-24 18:05:22 +0300

    update

--- app/views/site/layouts/header.blade.php
+++ app/views/site/layouts/header.blade.php
@@ -1,28 +1,73 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Новости</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+    <!--[if lt IE 9]>
+    <script src="/js/dist/html5shiv.js"></script>
+    <![endif]-->
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js" ></script>
+    <script type="text/javascript" src="/js/bootstrap.js"></script>
+</head>
+<body>
+<div class="wrapper">
+    <div class="ramka_top"></div>
+    <header>
+        <div class="site_name">
+            <p>Нам доверяют свои деньги</p>
+            <p>397<!-- вставить количество вкладчиков  --> партнеров</p>
+        </div>
+        <a href="" class="reg">Регистрация в проекте <!-- Личный кабинет --></a>
+        <!-- Контакты только на главной -->
+        @if(Request::is('/'))
+        <div class="contacts">
+            <h3>СВЯЗЬ С НАМИ</h3>
+            <ul>
+                <li>
+                    <p>Skype:</p>
+                    <p>happylineofmoney</p>
+                </li>
+                <li>
+                    <p>Skype:</p>
+                    <p>+91 9123456789</p>
+                </li>
+                <li>
+                    <p>Email:</p>
+                    <p>happyline@gmail.com</p>
+                </li>
+            </ul>
+        </div>
+        @endif
+        <!-- Конец контактов -->
+        <nav>
+            <div class="container">
+                <menu class="left">
+                    <li class="l1"><a href="{{URL::route('home')}}">ГЛАВНАЯ</a></li>
+                    <li class="l2">
+                        <a href="">МАРКЕТИНГ</a>
+                        <ul>
+                            <li><a href="#">Инвестиционный</a></li>
+                            <li><a href="#">Линейный</a></li>
+                        </ul>
+                    </li>
+                    <li class="l3"><a href="{{URL::route('faq')}}">FAQ</a></li>
+                    <li class="l4"><a href="">ПРАВИЛА</a></li>
+                </menu>
+                <menu class="right">
+                    <li class="l5"><a href={{URL::route('news')}}"">НОВОСТИ</a></li>
+                    <li class="l6"><a href="">ОТЗЫВЫ</a></li>
+                    <li class="l7"><a href="">КОНТАКТЫ</a></li>
+                </menu>
+                <div class="clearfix"></div>
+            </div>
 
-<nav>
-    <div class="container">
-        <menu class="left">
-            <li class="l1"><a href="{{URL::route('home')}}">ГЛАВНАЯ</a></li>
-            <li class="l2">
-                <a href="">МАРКЕТИНГ</a>
-                <ul>
-                    <li><a href="#">Инвестиционный</a></li>
-                    <li><a href="#">Линейный</a></li>
-                </ul>
-            </li>
-            <li class="l3"><a href="{{URL::route('faq')}}">FAQ</a></li>
-            <li class="l4"><a href="">ПРАВИЛА</a></li>
-        </menu>
-        <menu class="right">
-            <li class="l5"><a href={{URL::route('news')}}"">НОВОСТИ</a></li>
-            <li class="l6"><a href="">ОТЗЫВЫ</a></li>
-            <li class="l7"><a href="">КОНТАКТЫ</a></li>
-        </menu>
-        <div class="clearfix"></div>
-    </div>
+            <a href="" class="logo"></a>
+            <a href="" class="enter">Вход для клиентов <img src="images/lock.png" alt=""></a>
+            <!-- <a href="" class="enter">Выход <img src="images/lock.png" alt=""></a>  -->
 
-    <a href="" class="logo"></a>
-    <a href="" class="enter">Вход для клиентов <img src="images/lock.png" alt=""></a>
-    <!-- <a href="" class="enter">Выход <img src="images/lock.png" alt=""></a>  -->
-
-</nav>
+        </nav>
+    </header>
+    <!-- header END -->
