f1c6a8f5837e3f120771178f4ce86b677fe00aa4
sanfgroup <sanfgroup@gmail.com>
2013-10-24 15:33:21 +0300

    update

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('site.home');
-});
+Route::get('/', array('as'=>'home', 'uses'=>function()
+    {
+        return View::make('site.home');
+    }));
