4fd9af40d92abf2f4c2c36d9261f01c221b8bada
sanfgroup <sanfgroup@gmail.com>
2013-10-24 16:07:52 +0300

    update

--- app/views/site/layouts/home.blade.php
+++ app/views/site/layouts/home.blade.php
@@ -1,21 +1,24 @@
 @include("site.layouts.header")
 <div class="container">
+    <div class="container_all">
     @yield("content")
-
-<div class="sidebar">
-    <h2>НОВОСТИ</h2>
-    <div class="onepost">
-        <div class="post_date">
-            20 сентября 2013, 19:58
-        </div>
-        <h3>ПОЗДРАВЛЯЕМ С ОТКРЫТИЕМ ПРОЕКТА HAPPY LINE OF MONEY!!!</h3>
-        <div class="post_description">
-            <p>Уважаемые участники!</p>
-            <p>Мы рады сообщить радостную новость! Сегодня наш сайт запускается в 21.30 МСК ! Сейчас участники имеют возможность регистрироваться в проекте и привлекать по своей реферальной ссылке новых участников.</p>
-            <a href="#" class="read_more">Читать полностью ...</a>
-        </div>
     </div>
 
+    <div class="sidebar">
+        <h2>НОВОСТИ</h2>
+        <div class="onepost">
+            <div class="post_date">
+                20 сентября 2013, 19:58
+            </div>
+            <h3>ПОЗДРАВЛЯЕМ С ОТКРЫТИЕМ ПРОЕКТА HAPPY LINE OF MONEY!!!</h3>
+            <div class="post_description">
+                <p>Уважаемые участники!</p>
+                <p>Мы рады сообщить радостную новость! Сегодня наш сайт запускается в 21.30 МСК ! Сейчас участники имеют возможность регистрироваться в проекте и привлекать по своей реферальной ссылке новых участников.</p>
+                <a href="#" class="read_more">Читать полностью ...</a>
+            </div>
+        </div>
+
+    </div>
     <div class="clear"></div>
-    </div>
+</div>
 @include("site.layouts.footer");
