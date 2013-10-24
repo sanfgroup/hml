ebd6bd7d845d53cd37fcce0c3d96aaa72e2e7ee4
sanfgroup <sanfgroup@gmail.com>
2013-10-24 15:10:10 +0300

    update

--- app/views/site/layouts/default.blade.php
+++ app/views/site/layouts/default.blade.php
@@ -1,6 +1,5 @@
-@extends("site.layouts.header")
+@include("site.layouts.header")
     <div class="container_all">
         @yield("content")
     </div>
-
-@extends("site.layouts.footer");
+@include("site.layouts.footer");
