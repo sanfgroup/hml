86e37512b185e14c3f282fa5636be432c9f4506e
sanfgroup <sanfgroup@gmail.com>
2013-10-27 17:53:09 +0200

    update

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -13,21 +13,21 @@ class UserController extends BaseController {
     }
 
     public function postRegistration() {
-        $input = Input::all();//
+        $input = Input::all();
         $rules = array(
-            'fio' => 'Required|Min:3|Max:80|Alpha',
-            'email'     => 'Required|Between:3,64|Email|Unique:users',
-            'username'       => 'Integer|Min:18',
-            'password'  =>'Required|min:6|Confirmed',
-            'password_confirmation'=>'Required|min:6'
+            'fio'                   => 'Required|Min:3|Max:80|Alpha',
+            'email'                 => 'Required|Between:3,64|Email|Unique:users',
+            'username'              => 'required|Unique:users',
+            'password'              => 'Required|min:6|Confirmed',
+            'password_confirmation' => 'Required|min:6'
         );
         $v = Validator::make($input, $rules);
         if( $v->passes() ) {
             User::create(array(
-                'username' => $input['username'],
-                'email' => $input['username'],
-                'password' => Hash::make($input['username']),
-                'fio' => $input['username'],
+                'username'  => $input['username'],
+                'email'     => $input['username'],
+                'password'  => Hash::make($input['username']),
+                'fio'       => $input['username'],
             ));
         } else {
             return Redirect::route('home')
