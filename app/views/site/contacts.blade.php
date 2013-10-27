07aaec4a71cff14bf61e4cf9b1275f911b67b792
sanfgroup <sanfgroup@gmail.com>
2013-10-27 19:26:49 +0200

    contacts

--- app/views/site/contacts.blade.php
+++ app/views/site/contacts.blade.php
@@ -10,9 +10,18 @@
 <p><strong>А также, с помощью формы обратной связи:</strong></p>
 <br>
 <div class="contact_form">
+    <div class="contactsName">
+        Форма обратной связи
+    </div>
     <form action="">
-
+        <ul>
+            <li><span>Ваше имя:</span><input type="text" name="your_name" id="your_name" class="form-control"></li>
+            <li><span>Ваш e-mail:</span><input type="text" name="your_email" id="your_email" class="form-control"></li>
+            <li><span>Тема:</span><input type="text" name="item" id="item" class="form-control"></li>
+            <li><span>Сообщение:</span><textarea name="message" id="message" class="form-control" ></textarea></li>
+        </ul>
+        <button type="submit" >Отправить</button>
+        <div class="clear"></div>
     </form>
 </div>
-
 @stop
