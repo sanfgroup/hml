a51fd8879e38114ae5aa610075a4a346679e372b
sanfgroup <sanfgroup@gmail.com>
2013-11-01 10:50:37 +0200

    доверстка3

---
+++ app/views/site/reviews.add.blade.php
@@ -0,0 +1,13 @@
+@extends('site.layouts.default')
+
+@section('content')
+<h2>Оставить отзыв</h2>
+{{ Form::open() }}
+<div class="add_review">
+    <textarea name="add_review" id="add_review" ></textarea>
+    <div class="foot_r">
+        <input type="submit" class="btn btn-primary" value="Оставить отзыв" />
+    </div>
+</div>
+{{Form::close()}}
+@stop
