4495fe88f2e233d188f426e4eb5f9b56782870df
sanfgroup <sanfgroup@gmail.com>
2013-11-04 12:24:02 +0200

    доверстка3

--- app/views/site/private_header.blade.php
+++ app/views/site/private_header.blade.php
@@ -8,14 +8,15 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">Пополнение баланса</h4>
+<!--                <h4 class="modal-title">Пополнение баланса</h4>-->
+                <h4>Пополнение счета будет возможно после открытия проекта</h4>
             </div>
-            <div class="modal-body">
+   <!--         <div class="modal-body">
                 <div>Сумма</div>
 
                 <div class="payf">
-                    {{$form}}
-                    {{$form2}}
+                    $form
+                    $form2
                 </div>
                 <div>С платежной системы</div>
                 <button class="btn btn-primary disabled" id="addPerfect">Perfectmoney</button>
@@ -26,7 +27,7 @@
                 <button id="pay" class="btn btn-warning">
                     Пополнить
                 </button>
-            </div>
+            </div>-->
         </div>
     </div>
 </div>
@@ -35,28 +36,30 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">Пополнение баланса</h4>
-            </div>
-            {{Form::open(array('url'=>''))}}
-            <div class="modal-body">
-                <div>Сумма</div>
-                <input type="text" name="summ"/>
-                <div>С платежной системы</div>
-                <div class="btn-group" data-toggle="buttons">
-                    <label class="btn btn-primary active">
-                        <input type="radio" checked="checked" name="system" value="{{Auth::user()->perfectmoney}}"> Perfectmoney
-                    </label>
-                    <label class="btn btn-primary">
-                        <input type="radio" name="system" value="{{Auth::user()->okpay}}"> OKPAY
-                    </label>
-                </div>
+                <h4 class="modal-title">Вывод средств</h4>
+                <h4 class="modal-title">Вывод средств будет доступен после открытия пороекта</h4>
 
             </div>
-            <div class="modal-footer">
-                <button id="pay" type="submit" class="btn btn-warning">
-                    Вывести
-                </button>
-            </div>
+            {{Form::open(array('url'=>''))}}
+<!--            <div class="modal-body">-->
+<!--                <div>Сумма</div>-->
+<!--                <input type="text" name="summ" class="form-control"/>-->
+<!--                <div>С платежной системы</div>-->
+<!--                <div class="btn-group" data-toggle="buttons">-->
+<!--                    <label class="btn btn-primary active">-->
+<!--                        <input type="radio" checked="checked" name="system" value="Auth::user()->perfectmoney"> Perfectmoney-->
+<!--                    </label>-->
+<!--                    <label class="btn btn-primary">-->
+<!--                        <input type="radio" name="system" value="Auth::user()->okpay"> OKPAY-->
+<!--                    </label>-->
+<!--                </div>-->
+<!---->
+<!--            </div>-->
+<!--            <div class="modal-footer">-->
+<!--                <button id="pay" type="submit" class="btn btn-warning">-->
+<!--                    Вывести-->
+<!--                </button>-->
+<!--            </div>-->
             {{Form::close()}}
         </div>
     </div>
