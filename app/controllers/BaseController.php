5bcd8b08e42d69a91590e992a85c11918fc71e78
vinnizp <vinnizp@gmail.com>
2013-11-05 12:11:02 +0200

    1

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -12,29 +12,29 @@ class BaseController extends Controller {
 	protected function setupLayout()
 	{
 
-        $this->user = null;
-        if(!Auth::guest()) {
-            $this->user = Auth::user();
-        }
 
         View::share('user', $this->user);
         if ( ! is_null($this->layout))
         {
             $this->layout = View::make($this->layout);
         }
-        $uid = $this->user->id;
-        $pay = $this->user->pay;
-        View::composer('site.private_header', function($view) use($uid, $pay)
-        {
-            $data = array();
-            if(!Auth::guest()) {
-                $pm = new PerfectMoney();
-                $data['form'] = $pm->form($uid, $pay);
-                $ok = new OkPay();
-                $data['form2'] = $ok->form($uid, $pay);
-            }
-            $view->with($data);
-        });
+        $this->user = null;
+        if(!Auth::guest()) {
+            $this->user = Auth::user();
+            $uid = $this->user->id;
+            $pay = $this->user->pay;
+            View::composer('site.private_header', function($view) use($uid, $pay)
+            {
+                $data = array();
+                if(!Auth::guest()) {
+                    $pm = new PerfectMoney();
+                    $data['form'] = $pm->form($uid, $pay);
+                    $ok = new OkPay();
+                    $data['form2'] = $ok->form($uid, $pay);
+                }
+                $view->with($data);
+            });
+        }
 	}
 
 }
