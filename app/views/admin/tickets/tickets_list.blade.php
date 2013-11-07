a7598ab94151f449d1ba072d041431b80529b1e3
sanfgroup <sanfgroup@gmail.com>
2013-11-07 17:11:52 +0200

    доверстка3

--- app/views/admin/tickets/tickets_list.blade.php
+++ app/views/admin/tickets/tickets_list.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 @foreach($tickets as $ticket)
-<section @if($ticket->thread == 0) style="padding: 10px; border-radius: 5px; background: #abebff;" @endif >
+<section style=" margin: 10px 0;  padding: 10px; border-radius: 5px; @if($ticket->thread == 0) background: #F09D6F; @elseif($ticket->thwrite == 1) background: #97CC8A;  @elseif($ticket->thread == 1) background: #abebff;  @endif " >
     <h3><a href="{{URL::route('admin.ticket', array($ticket->id))}}">{{$ticket->item}}</a></h3>
-    <p>{{$ticket->created_at}}</p>
+    <p>{{$ticket->created_at}} @if($ticket->thwrite == 1) | Отвечено @endif </p>
     <div>{{$ticket->message}}</div>
 </section>
 @endforeach
-
+{{$tickets->links()}}
 @stop
